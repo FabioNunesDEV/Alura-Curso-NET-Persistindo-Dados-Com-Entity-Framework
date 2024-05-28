60e69566ae41b1e47e983c985f1ba5c75606aeca
Fábio Nunes <fabionunesbr@gmail.com>
2024-05-28 15:00:05 -0300

    atualização

--- ScreenSound/obj/Debug/net6.0/ScreenSound.AssemblyInfo.cs
+++ ScreenSound/obj/Debug/net6.0/ScreenSound.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+328e34b4038928e5898eb1b31b41fe343c48a1a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ea751127736b5c6ccafa9d17235a3e5153e3665")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
